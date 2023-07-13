b8efa8b76dda613d8f3b7fdd00981bade1c88cdc
Philip Tambè <33310992+PhilipTamb@users.noreply.github.com>
2023-07-13 20:05:54 +0200

    Add files via upload

---
+++ target/debug/bridge-c.d
@@ -0,0 +1,1 @@
+/Users/philip/dev/rust-practice/c-function/bridge-statical/bridge-c/target/debug/bridge-c: /Users/philip/dev/rust-practice/c-function/bridge-statical/bridge-c/build.rs /Users/philip/dev/rust-practice/c-function/bridge-statical/bridge-c/src/main.rs
