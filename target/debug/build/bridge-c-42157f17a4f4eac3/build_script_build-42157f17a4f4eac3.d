68a8ae63dce18e85222769d10ad60a0eee3d8db7
Philip Tambè <33310992+PhilipTamb@users.noreply.github.com>
2023-07-13 20:06:35 +0200

    Add files via upload

---
+++ target/debug/build/bridge-c-42157f17a4f4eac3/build_script_build-42157f17a4f4eac3.d
@@ -0,0 +1,5 @@
+/Users/philip/dev/rust-practice/c-function/bridge-example1/bridge-c/target/debug/build/bridge-c-42157f17a4f4eac3/build_script_build-42157f17a4f4eac3: build.rs
+
+/Users/philip/dev/rust-practice/c-function/bridge-example1/bridge-c/target/debug/build/bridge-c-42157f17a4f4eac3/build_script_build-42157f17a4f4eac3.d: build.rs
+
+build.rs:
